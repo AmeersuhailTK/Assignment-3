8580b6df9dc699d710951a5b18220ecc794349c5
Disney <disneywelcome4062@gmail.com>
2026-05-12 09:41:08 +0300

    second

--- src/Array/Largest_Smallest.java
+++ src/Array/Largest_Smallest.java
@@ -1,5 +1,25 @@
 package Array;
 
 public class Largest_Smallest {
+	public static void main(String[] args) {
 
+        int[] arr = {67, 45, 34, 12, 2};
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+
+            if (arr[i] < smallest)
+                smallest = arr[i];
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Smallest = " + smallest);
+    }
 }
+
+
+
